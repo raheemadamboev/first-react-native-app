52cc7c295861a3b955e6e4479407a820208714c3
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-24 13:14:21 +0500

    using state and button click

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outputButtonText, setButtonText] = useState("First state");
+  const [status, setStatus] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{outputButtonText}</Text>
+      <Button
+        title="Change text!"
+        onPress={() => {
+          if (status) {
+            setButtonText("State changed!");
+            setStatus(false);
+          } else {
+            setButtonText("First state");
+            setStatus(true);
+          }
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
